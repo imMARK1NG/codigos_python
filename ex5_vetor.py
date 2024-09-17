b473856259f6de0a74ad4c4cfa8b54bfbc0a49e8
imMARK1NG <snitram47@gmail.com>
2024-09-17 20:54:37 -0300

    modified:   ex5_vetor.py 	modified:   ex6_vetor.py

--- ex5_vetor.py
+++ ex5_vetor.py
@@ -1,23 +1,23 @@
-tamanho = int(input("Digite o tamanho do vetor:"))
-vetor = [int] * tamanho
+tamanho = int(input("Digite o tamanho do vetor:")) #atribui um valor n para a variavel tamanho.
+vetor = [int] * tamanho #declarando um vetor de numeros inteiros, * tamanho multiplica a quantidade de valores que teram dentro do vetor.
 
-for i in range(len(vetor)):
-    vetor[i] = int(input(f"Digite o {i + 1} valor:"))
+for i in range(len(vetor)): #laço para percorrer o vetor.
+    vetor[i] = int(input(f"Digite o {i + 1} valor:")) #atribui valores n valores inteiros no vetor.
 
-maior_valor = vetor[0]
-menor_valor = vetor[0]
+maior_valor = vetor[0] #atribuindo a posição zero do vetor a variavel maior_valor.
+menor_valor = vetor[0] #atribuindo a posição zero do vetor a variavel menor_valor.
 
-for i in range(len(vetor)):
-    if vetor[i] > maior_valor:
-        maior_valor = vetor[i]
-    if vetor[i] < menor_valor:
-        menor_valor = vetor[i]
+for i in range(len(vetor)): #laço pra percorrer o vetor.
+    if vetor[i] > maior_valor: #condição para saber se o valor do vetor é maior que o valor q tá na variavel maior_valor.
+        maior_valor = vetor[i] #caso seja verdade a variavel maior_valor recebe o valor do vetor[i].
+    if vetor[i] < menor_valor: #condição para saber se o valor do vetor é menor que o valor q tá na variavel menor_valor.
+        menor_valor = vetor[i] #caso seja verdade a variavel menor_valor recebe o valor do vetor[i].
 
-print("Dados do vetor:\n")
-for i in range(len(vetor)):
-    print(vetor[i], end=" ")
+print("Dados do vetor:\n") #imprime os dados que estão entre aspas na tela.
+for i in range(len(vetor)): #laço pra pecorrer o vetor.
+    print(vetor[i], end=" ") #imprime o dados que estão guardados dentro do vetor[i] na tela;
 
-print("\n")
+print("\n") #quebra de linha
 
-print(f"O maior valor do vetor é {maior_valor}\n")
-print(f"O menor valor do vetor é {menor_valor}")
+print(f"O maior valor do vetor é {maior_valor}\n") #imprime os dados que estão entre aspas e os dados da variavel maior_valor na tela.
+print(f"O menor valor do vetor é {menor_valor}") #imprime os dados que estão entre aspas e os dados da variavel menor_valor na tela.
--- ex6_vetor.py
+++ ex6_vetor.py
@@ -1,9 +1,9 @@
-numero_de_elementos = int(input("Digite o numero de elementos:"))
-vetor = [i + 1 for i in range(numero_de_elementos)]
-soma = 0
+numero_de_elementos = int(input("Digite o numero de elementos:")) #atribui n valores vetor, informados pelo usuario.
+vetor = [i + 1 for i in range(numero_de_elementos)]  #atribui n valores inteiros dentro do vetor.
+soma = 0 #declarando a variavel soma e inicializando ela com o valor zero.
 
-for i in range(len(vetor)):
-    soma += i + 1
+for i in range(len(vetor)): #laço para percorrer o vetor.
+    soma += i + 1 #pega o indice i e soma com mais 1.
 
-print(vetor)
-print(soma)
+print(vetor) #imprime os dados da variavel vetor e mostra na tela.
+print(soma) #imprime os dados da variavel soma e mostra na tela.
